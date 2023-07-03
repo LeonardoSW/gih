52d946d18643f4492d03a64a32c57f1dc0572c6b
Leonardo Silva Walder <42324501+LeonardoSW@users.noreply.github.com>
2023-07-03 19:09:11 -0300

    upload project gih

---
+++ Controllers/VendaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendaController : ControllerBase
+    {
+        public VendaController()
+        {
+        }
+
+        [HttpDelete]
+        public IActionResult ExcluirVenda()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Models/ProdutoInputModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Aula2.Models
+{
+    public class ProdutoInputModel
+    {
+        public string NomeProduto { get; set; }
+        public string Marca { get; set; }
+    }
+}
